183674bdb42e3d551fe462fd4ace8bdc40f9bd80
Nicolaijo <nuster200569@gmail.com>
2025-01-29 12:34:42 +0100

    Oppgave 1.2 og 1.3
    Glemte å laste opp 1.2 i går natt
    Begynte på 1.3 i dag tildig

---
+++ Oblig1/src/no/hvl/data102/filmarkiv/adt/FilmarkivADT.java
@@ -0,0 +1,64 @@
+package no.hvl.data102.filmarkiv.adt;
+
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+
+/**
+ * Et grensesnitt for filmarkiv med nødvendige operasjoner.
+ */
+public interface FilmarkivADT {
+
+    /**
+     * Henter en film med gitt nummer fra arkivet.
+     * 
+     * @param nr nummeret på filmen som skal hentes.
+     * @return filmen med gitt nummer, eller null hvis den ikke finnes.
+     */
+    Film finnFilm(int nr);
+
+    /**
+     * Legger til en ny film i arkivet.
+     * 
+     * @param nyFilm filmen som skal legges til.
+     */
+    void leggTilFilm(Film nyFilm);
+
+    /**
+     * Sletter en film med gitt nummer.
+     * 
+     * @param filmnr nummeret på filmen som skal slettes.
+     * @return true hvis filmen ble slettet, false ellers.
+     */
+    boolean slettFilm(int filmnr);
+
+    /**
+     * Søker etter filmer som inneholder en gitt delstreng i tittelen.
+     * 
+     * @param delstreng delstrengen som må være i tittelen.
+     * @return en tabell med filmer som inneholder delstrengen i tittelen.
+     */
+    Film[] soekTittel(String delstreng);
+
+    /**
+     * Søker etter filmer som inneholder en gitt delstreng i produsentens navn.
+     * 
+     * @param delstreng delstrengen som må være i produsentens navn.
+     * @return en tabell med filmer som inneholder delstrengen i produsentens navn.
+     */
+    Film[] soekProdusent(String delstreng);
+
+    /**
+     * Finner antall filmer av en gitt sjanger.
+     * 
+     * @param sjanger sjangeren som skal telles.
+     * @return antall filmer av gitt sjanger.
+     */
+    int antall(Sjanger sjanger);
+
+    /**
+     * Finner totalt antall filmer i arkivet.
+     * 
+     * @return antall filmer i arkivet.
+     */
+    int antall();
+}
---
+++ Oblig1/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -0,0 +1,106 @@
+package no.hvl.data102.filmarkiv.impl;
+
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+import java.util.Arrays;
+
+public class Filmarkiv implements FilmarkivADT {
+    private Film[] filmer;
+    private int antall;
+
+    /**
+     * 
+     * 
+     * @param 
+     */
+    public Filmarkiv(int kapasitet) {
+        filmer = new Film[kapasitet];
+        antall = 0;
+    }
+
+    @Override
+    public Film finnFilm(int nr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getFilmnr() == nr) {
+                return filmer[i];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+        if (antall == filmer.length) {
+            utvid();
+        }
+        filmer[antall++] = nyFilm;
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getFilmnr() == filmnr) {
+                filmer[i] = filmer[antall - 1]; 
+                filmer[antall - 1] = null;
+                antall--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        Film[] treff = new Film[antall];
+        int teller = 0;
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getTittel().toLowerCase().contains(delstreng.toLowerCase())) {
+                treff[teller++] = filmer[i];
+            }
+        }
+        return trimTabell(treff, teller);
+    }
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        Film[] treff = new Film[antall];
+        int teller = 0;
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getProdusent().toLowerCase().contains(delstreng.toLowerCase())) {
+                treff[teller++] = filmer[i];
+            }
+        }
+        return trimTabell(treff, teller);
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        int teller = 0;
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getSjanger() == sjanger) {
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    @Override
+    public int antall() {
+        return antall;
+    }
+
+
+    private void utvid() {
+        filmer = Arrays.copyOf(filmer, filmer.length * 2);
+    }
+
+    /**
+     * 
+     * 
+     * @param tabell  original tabell.
+     * @param lengde  antall elementer som skal beholdes.
+     * @return ny tabell med riktig størrelse.
+     */
+    private Film[] trimTabell(Film[] tabell, int lengde) {
+        return Arrays.copyOf(tabell, lengde);
+    }
+}
